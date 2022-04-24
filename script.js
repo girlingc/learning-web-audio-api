511b602f351cf5a4287487cdef91e6e1abc06901
Christian Girling <girling.christian@gmail.com>
2022-04-23 19:43:48 -0700

    Setup script

---
+++ script.js
@@ -0,0 +1,5 @@
+const volume = document.getElementById("volume");
+const bass = document.getElementById("bass");
+const mid = document.getElementById("mid");
+const treble = document.getElementById("treble");
+const visualizer = document.getElementById("visualizer");
