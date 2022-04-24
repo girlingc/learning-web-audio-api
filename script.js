35474d12c265a31188ef88b64f588e6715329c24
Christian Girling <girling.christian@gmail.com>
2022-04-23 19:47:42 -0700

    Setup getAudio

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const bass = document.getElementById("bass");
 const mid = document.getElementById("mid");
 const treble = document.getElementById("treble");
 const visualizer = document.getElementById("visualizer");
+
+const context = new AudioContext();
+
+function setupContext() {}
+
+function getAudio() {
+  return navigator.getUserMedia({
+    audio: {
+      echoCancellation: false,
+      autoGainControl: false,
+      noiseSuppression: false,
+      latency: 0,
+    },
+  });
+}
