ea440d69581b4e6d31b8d5e2ae5138472f9d5681
Christian Girling <girling.christian@gmail.com>
2022-04-24 19:39:18 -0700

    Change variable names to be more specific

--- script.js
+++ script.js
@@ -23,9 +23,7 @@ const init = () => {
         visualize();
       })
       .catch((err) => {
-        alert(
-          "Sorry, microphone permissions are required for the app. Feel free to read on without playing :)"
-        );
+        alert("Sorry, microphone permissions are required for the app.");
       });
   }
 
@@ -238,51 +236,51 @@ const autoCorrelate = (buffer, sampleRate) => {
   }
 
   // Find a range in the buffer where the values are below a given threshold.
-  let r1 = 0;
-  let r2 = SIZE - 1;
+  let range1 = 0;
+  let range2 = SIZE - 1;
   const threshold = 0.2;
 
-  // Walk up for r1
+  // Walk up for range1
   for (let i = 0; i < SIZE / 2; i++) {
     if (Math.abs(buffer[i]) < threshold) {
-      r1 = i;
+      range1 = i;
       break;
     }
   }
 
-  // Walk down for r2
+  // Walk down for range2
   for (let i = 1; i < SIZE / 2; i++) {
     if (Math.abs(buffer[SIZE - i]) < threshold) {
-      r2 = SIZE - i;
+      range2 = SIZE - i;
       break;
     }
   }
 
   // Trim the buffer to these ranges and update SIZE.
-  buffer = buffer.slice(r1, r2);
+  buffer = buffer.slice(range1, range2);
   SIZE = buffer.length;
 
   // Create a new array of the sums of offsets to do the autocorrelation
-  const c = new Array(SIZE).fill(0);
+  const offsetsArray = new Array(SIZE).fill(0);
   // For each potential offset, calculate the sum of each buffer value times its offset value
   for (let i = 0; i < SIZE; i++) {
     for (let j = 0; j < SIZE - i; j++) {
-      c[i] = c[i] + buffer[j] * buffer[j + i];
+      offsetsArray[i] = offsetsArray[i] + buffer[j] * buffer[j + i];
     }
   }
 
   // Find the last index where that value is greater than the next one (the dip)
-  let d = 0;
-  while (c[d] > c[d + 1]) {
-    d++;
+  let dip = 0;
+  while (offsetsArray[dip] > offsetsArray[dip + 1]) {
+    dip++;
   }
 
   // Iterate from that index through the end and find the maximum sum
   let maxValue = -1;
   let maxIndex = -1;
-  for (let i = d; i < SIZE; i++) {
-    if (c[i] > maxValue) {
-      maxValue = c[i];
+  for (let i = dip; i < SIZE; i++) {
+    if (offsetsArray[i] > maxValue) {
+      maxValue = offsetsArray[i];
       maxIndex = i;
     }
   }
@@ -294,12 +292,12 @@ const autoCorrelate = (buffer, sampleRate) => {
   // interpolation is parabolic interpolation. It helps with precision. We suppose that a parabola pass through the
   // three points that comprise the peak. 'a' and 'b' are the unknowns from the linear equation system and b/(2a) is
   // the "error" in the abscissa. Well x1,x2,x3 should be y1,y2,y3 because they are the ordinates.
-  const x1 = c[T0 - 1];
-  const x2 = c[T0];
-  const x3 = c[T0 + 1];
+  const xOrdinate1 = offsetsArray[T0 - 1];
+  const xOrdinate2 = offsetsArray[T0];
+  const xOrdinate3 = offsetsArray[T0 + 1];
 
-  const a = (x1 + x3 - 2 * x2) / 2;
-  const b = (x3 - x1) / 2;
+  const a = (xOrdinate1 + xOrdinate3 - 2 * xOrdinate2) / 2;
+  const b = (xOrdinate3 - xOrdinate1) / 2;
   if (a) {
     T0 = T0 - b / (2 * a);
   }
